9b11c0f9b903e0a5b5102e0c3a1fd5ab9fdba4e6
Grace Shih <vocalise@gmail.com>
2018-08-01 04:46:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,7 @@ class ApplicationController < Sinatra::Base
 		@user = User.find_by(username: params[:username])
 		if @user && @user.authenticate(params[:password])
 		  session[:id] = @user.id
+		end
 	end
 
 	get "/success" do
