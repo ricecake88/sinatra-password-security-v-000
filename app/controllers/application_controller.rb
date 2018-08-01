56d263642493bed8a5eae83727a4e1196875b18f
Grace Shih <vocalise@gmail.com>
2018-08-01 04:41:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-	  @user = User.create(username: params[:username] password_digest: params[:password])
+	  @user = User.create(username: params[:username] password: params[:password])
 	  if @user
 	    @user.save
 	    session[:id] = @user.id
